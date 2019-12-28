b7f48df16bff90aa7cd91ba15053ce7820def448
Everton Kunz <ehokeverton@gmail.com>
2019-12-28 16:01:54 -0400

    Adiciona todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Todo } from "src/models/todos";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'todoApp';
+  public title = "Minhas Tarefas";
+  public todos: Todo[] = [];
+
+  constructor() {
+    this.todos.push(new Todo(1, "cortar a grama", true));
+    this.todos.push(new Todo(2, "ir ao supermercado", false));
+    this.todos.push(new Todo(3, "banhar o cachorro", false));
+  }
 }
