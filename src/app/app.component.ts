c89742716823d9494625075fab544ccc005a82b0
Everton Kunz <ehokeverton@gmail.com>
2019-12-29 18:01:45 -0400

    Adiciona persistencia dos dados com localStorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,12 +20,15 @@ export class AppComponent {
         Validators.required
       ])]
     });
+
+    this.load();
   }
 
   add() {
     const title = this.form.controls['title'].value;
     const id = this.todos.length + 1;
     this.todos.push(new Todo(id, title, false));
+    this.save();
     this.form.reset();
   }
 
@@ -35,13 +38,30 @@ export class AppComponent {
     if (index != -1) {
       this.todos.splice(index, 1);
     }
+
+    this.save();
   }
 
   markAsDone(todo: Todo) {
     todo.done = true;
+    this.save();
   }
 
   markAsUndone(todo: Todo) {
     todo.done = false;
+    this.save();
   }
+
+  save() {
+    const data = JSON.stringify(this.todos);
+
+    localStorage.setItem('todos', data);
+  }
+
+  load() {
+    const data = localStorage.getItem('todos');
+
+    this.todos = JSON.parse(data);
+  }
+
 }
